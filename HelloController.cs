59e455c70dc158ce5d93db96e876045777013bfa
junxian428 <58724748+junxian428@users.noreply.github.com>
2021-01-31 14:10:24 +0000

    Add files via upload

---
+++ HelloController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace FIRSTWEBAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+
+        [HttpPost]
+        public IActionResult Post(JObject payload)
+        {
+            return Ok(payload);
+        }
+    }
+}
